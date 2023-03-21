330f2e7eb94f6c9a75b7fe90760919150520eb3a
Patryk Ruczynski <ptrkk17@gmail.com>
2023-03-21 20:33:46 +0100

    Upadtes

--- app.js
+++ app.js
@@ -9,8 +9,26 @@ hamburger.onclick = function () {
 
 // Script for hero typing text
 const typed = new Typed('.pinput', {
-  strings: ['Frontend Developer', 'Freelancer', 'Programist in Your Company'],
-  typeSpeed: 220,
-  backSpeed: 170,
+  strings: ['Frontend Developer', 'Programist in Your Company 😉'],
+  typeSpeed: 70,
+  backSpeed: 70,
   loop: true,
+});
+
+// Redirecting to projects
+
+const redirect1 = document.querySelector('#trainer');
+const redirect2 = document.querySelector('#darcy');
+const redirect3 = document.querySelector('#challange');
+
+redirect1.addEventListener('click', () => {
+  window.location.href = 'https://ruczynskipatryk.pl';
+});
+
+redirect2.addEventListener('click', () => {
+  window.location.href = 'https://minikoparka-argon.pl';
+});
+
+redirect3.addEventListener('click', () => {
+  window.location.href = 'https://zmianana100.pl';
 });
